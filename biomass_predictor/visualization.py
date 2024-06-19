12ad758f9c267825cabe6d9bb0b16ac66d4e5ef0
Rodrigo Parra <rodpar07@gmail.com>
2024-06-19 11:48:20 -0400

    Add correlation analysis

--- biomass_predictor/visualization.py
+++ biomass_predictor/visualization.py
@@ -1,8 +1,33 @@
 import data
 import matplotlib.pyplot as plt
+import seaborn as sn
 
-if __name__ == "__main__":
-    df = data._get_biomass_growth_data()
+
+def visualize_train_data():
+    df = data.get_growth_dataset()
     ax = df.plot.scatter(x="previous_biomass", y="biomass", c="DarkBlue")
     ax = df.plot.hist(column="biomass_change")
     plt.show()
+
+
+def visualize_correlation_matrix():
+    df = data.get_growth_dataset()
+    df = df[
+        [
+            "previous_biomass",
+            "cumulative_temperature",
+            "cumulative_humidity",
+            "cumulative_rain",
+            "cumulative_light",
+            "biomass",
+        ]
+    ]
+    corr_matrix = df.corr()  # type: ignore
+    ax = sn.heatmap(corr_matrix, annot=True)
+    plt.tight_layout()
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    visualize_correlation_matrix()
